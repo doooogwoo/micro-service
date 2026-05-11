9947ab5dbcf03334ed86f76d4b62b117f425625d
tiramisu <tiramisu@gmail.com>
2026-05-11 12:32:49 +0800

    feat(log file):將log輸出成檔案，並使用config server統一管理

--- user/src/main/java/com/ecommerce/user/controller/UserController.java
+++ user/src/main/java/com/ecommerce/user/controller/UserController.java
@@ -4,6 +4,9 @@ import com.ecommerce.user.dto.UserRequest;
 import com.ecommerce.user.dto.UserResponse;
 import com.ecommerce.user.service.UserService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,9 +16,11 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/users")
+@Slf4j
 public class UserController {
 
     private final UserService userService;
+    //private static Logger logger = LoggerFactory.getLogger(UserController.class);
 
     @GetMapping
     public ResponseEntity<List<UserResponse>> getAllUsers(){
@@ -25,6 +30,12 @@ public class UserController {
 
     @GetMapping("/{id}")
     public ResponseEntity<UserResponse> getUser(@PathVariable String id){
+        log.info("Received request to fetch user with id: {}", id);
+        log.debug("Fetching user details for id: {}", id);
+        log.error("This is a sample error log for user id: {}", id);
+        log.warn("This is a sample warning log for user id: {}", id);
+        log.trace("This is a sample trace log for user id: {}", id);
+
         return userService.fetchUser(id)
                 .map(ResponseEntity::ok)
                 .orElseGet(() -> ResponseEntity.notFound().build());
